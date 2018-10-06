6d5b6f4533e1dab1a7faf06ad3e8d05bea63b5ed
marvin evins <maevins@cps.edu>
2018-10-06 14:28:49 -0500

    setup express

--- app.js
+++ app.js
@@ -3,16 +3,32 @@ let Blockchain = require('./blockchain')
 
 let Transaction = require('./transaction')
 
-let transaction = new Transaction('Mary', 'Jerry', 100)
+const express = require('express')
+const app = express()
 
-let genesisBlock = new Block()
-let blockchain = new Blockchain(genesisBlock)
+app.get('/', function(req, res){
+  res.send("hello world")
+})
 
-let block = blockchain.getNextBlock([transaction])
-blockchain.addBlock(block)
 
-let anotherTransaction = new Transaction("Azam", "Jerry", 10)
-let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
-blockchain.addBlock(block1)
+app.get('/blockchain', function(req, res){
 
-console.log(blockchain)
+
+  let transaction = new Transaction('Mary', 'Jerry', 100)
+
+  let genesisBlock = new Block()
+  let blockchain = new Blockchain(genesisBlock)
+
+  let block = blockchain.getNextBlock([transaction])
+  blockchain.addBlock(block)
+
+  let anotherTransaction = new Transaction("Azam", "Jerry", 10)
+  let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
+  blockchain.addBlock(block1)
+
+  res.json(blockchain)
+})
+
+app.listen(3000, function(req, res){
+  console.log("server has started")
+})
