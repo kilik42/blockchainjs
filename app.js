a4b4c798c5cb804fc0e64542508351367cb77ba0
marvin evins <maevins@cps.edu>
2018-10-06 15:01:09 -0500

    adding transactions

--- app.js
+++ app.js
@@ -5,11 +5,37 @@ let Transaction = require('./transaction')
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json())
+let transactions = []
+
 
 app.get('/', function(req, res){
   res.send("hello world")
 })
 
+app.post('/transaction', function(req, res){
+
+  let to = req.body.to
+  let from = req.body.from
+  let amount = req.body.amount
+
+  let transaction = new Transaction(from, to, amount)
+
+  transactions.push(transaction)
+
+
+     console.log(req.body.to)
+     console.log(req.body.from)
+     console.log(req.body.amount)
+
+     res.json(transactions)
+
+
+
+})
 
 app.get('/blockchain', function(req, res){
 
@@ -28,6 +54,8 @@ app.get('/blockchain', function(req, res){
 
   res.json(blockchain)
 })
+
+
 
 app.listen(3000, function(req, res){
   console.log("server has started")
